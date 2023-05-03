4b98fad5be65fbab18f2d7adfdc1d366a42ae822
Tobias Jünger <tobias-juenger@web.de>
2023-05-03 14:20:42 +0200

    Array Übung

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let students=[];
+let firstStudent="Max";
+let secondStudent="Tom";
+let thirdStudent="Joe";
+
+students.push(firstStudent, secondStudent, thirdStudent);
+
+student.foreach( let  =>()
+{
+  print("<li>"+firstStudent,"</li>");
+  print("<li>"+secondStudent,"</li>");
+  print("<li>"+thirdStudent,"</li>");
+
+}
